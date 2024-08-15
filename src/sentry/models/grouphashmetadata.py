05f3680ef6d5e9078c41a8e1fa907282d7410085
Katie Byers <lobsterkatie@gmail.com>
2024-08-15 15:39:03 -0700

    add grouphash id and group id to `GroupHashMetadata` repr

--- src/sentry/models/grouphashmetadata.py
+++ src/sentry/models/grouphashmetadata.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 
 from sentry.backup.scopes import RelocationScope
 from sentry.db.models import Model, region_silo_model
+from sentry.db.models.base import sane_repr
 
 
 @region_silo_model
@@ -18,3 +19,9 @@ class GroupHashMetadata(Model):
     class Meta:
         app_label = "sentry"
         db_table = "sentry_grouphashmetadata"
+
+    @property
+    def group_id(self) -> int | None:
+        return self.grouphash.group_id
+
+    __repr__ = sane_repr("grouphash_id", "group_id")
